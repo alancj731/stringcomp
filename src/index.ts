482688f25ae73e1e4677ee52c2a05cd7872af6b0
alancj731 <winnipegdatafan@gmail.com>
2025-11-22 22:42:46 -0600

    change index.ts and remove main.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export * from "./main";
+import { extensions } from "./extension/index";
+
+type ExtensionsType = typeof extensions;
+
+declare global {
+    interface String extends ExtensionsType { }
+}
+
+const StringPrototype = String.prototype as any;
+
+for (const key of Object.keys(extensions)) {
+    StringPrototype.prototype[key] = function (...args: any[]) {
+        return extensions[key as keyof typeof extensions].apply(this, args);
+    };
+}
